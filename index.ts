02e05041889008a9cd48963b91c710aba87bd22c
huzaifah0x00 <huzaifah.asif.b@gmail.com>
2023-03-15 00:49:56 +0500

    initial commit ( added MeComFrame class )

---
+++ index.ts
@@ -0,0 +1,62 @@
+import crc16ccitt from "crc/crc16xmodem";
+
+export class MeComFrame {
+  // Frame structure:
+  // 1 Control  |  ASCII char | 8 Bits
+  // 2 Address  |  UINT8      | 16 Bits
+  // 3 Sequence |  Nr. UINT16 | 32 Bits
+  // 4 Payload  |  N * 8 Bits | N * 8 Bits
+  // 5 Frame    |  CRC UINT16 | 32 Bits
+  // 6 EOF      |  ASCII char | 8 Bits
+
+  private crc: number;
+  private EOF = "\r";
+
+  constructor(
+    public control = "#",
+    public address: number = 0,
+    public sequence: number = 0,
+    public payload: string
+  ) {
+    this.crc = this.calculateCRC(this.partialFrame);
+  }
+
+  private calculateCRC(data: string): number {
+    console.log(`doing ${data}`);
+    return crc16ccitt(Buffer.from(data));
+  }
+
+  /** Frame without CRC and EOF */
+  private get partialFrame(): string {
+    // make sure all values are specific number bits
+
+    const address = this.address.toString(16).padStart(2, "0");
+    const sequence = this.sequence.toString(16).padStart(4, "0");
+
+    let frame = "";
+
+    frame += this.control;
+    frame += address;
+    frame += sequence;
+    frame += this.payload;
+
+    return frame.toUpperCase();
+  }
+
+  public build(): string {
+    const address = this.address.toString(16).padStart(2, "0");
+    const sequence = this.sequence.toString(16).padStart(4, "0");
+    const crc = this.crc.toString(16).padStart(4, "0");
+
+    let frame = "";
+
+    frame += this.control;
+    frame += address;
+    frame += sequence;
+    frame += this.payload;
+    frame += crc;
+    frame += this.EOF;
+
+    return frame.toUpperCase();
+  }
+}
